de9dda07aebc5aa8017f11f31c0da7d2250a9643
Leon Kempers <lkempers@salesforce.com>
2020-12-22 09:12:27 -0800

    Updated YAML syntaxes

--- project_card-get-issue-dev/script.py
+++ project_card-get-issue-dev/script.py
@@ -1,15 +1,16 @@
 import os
 import re
 
+token = os.environ['INPUT_TOKEN']
 cardNote = os.environ['INPUT_CARDNOTE']
 issueUrl = os.environ['INPUT_ISSUEURL']
 
-print('Issue URL')
-print(issueUrl)
+print('Token')
+print(token)
 
 # Extract all email addresses from the Issue Note, store the last one
 #emailAddresses = re.findall(r'[\w\.-]+@[\w\.-]+', cardNote)
 #developer = emailAddresses[-1]
 
 # Print developer as output
-print(f"::set-output name=developer::{developer}")
+#print(f"::set-output name=developer::{developer}")
